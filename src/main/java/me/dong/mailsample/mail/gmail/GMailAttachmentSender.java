6bc90150c48e018e2fe7797d7787748ad3e24932
Hue Kim <opklnm102@gmail.com>
2018-10-28 16:00:08 +0900

    [feat] Add gmail attachment sender

---
+++ src/main/java/me/dong/mailsample/mail/gmail/GMailAttachmentSender.java
@@ -0,0 +1,57 @@
+package me.dong.mailsample.mail.gmail;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by huekim on 2018. 10. 27..
+ */
+@Component
+@Slf4j
+public class GMailAttachmentSender {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMessage(String to, String subject, String text, String pathToAttachment) {
+        sendMessage(Collections.singletonList(to), subject, text, Collections.singletonList(pathToAttachment));
+    }
+
+    public void sendMessage(String to, String subject, String text, List<String> pathToAttachments) {
+        sendMessage(Collections.singletonList(to), subject, text, pathToAttachments);
+    }
+
+    public void sendMessage(List<String> to, String subject, String text, String pathToAttachment) {
+        sendMessage(to, subject, text, Collections.singletonList(pathToAttachment));
+    }
+
+    public void sendMessage(List<String> to, String subject, String text, List<String> pathToAttachments) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+            messageHelper.setTo(to.toArray(new String[0]));
+            messageHelper.setSubject(subject);
+            messageHelper.setText(text);
+
+            for (String pathToAttachment : pathToAttachments) {
+                FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+                messageHelper.addAttachment(file.getFilename(), file);
+            }
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            log.error("fail send message ", e);
+        }
+    }
+}
---
+++ src/test/java/me/dong/mailsample/mail/gmail/GMailAttachmentSenderTest.java
@@ -0,0 +1,79 @@
+package me.dong.mailsample.mail.gmail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by huekim on 2018. 10. 27..
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GMailAttachmentSenderTest {
+
+    @Autowired
+    private GMailAttachmentSender sut;
+
+    @Test
+    public void 단일_수신자에게_단건의_첨부파일_전송시_성공() {
+        // given :
+        String to = "opklnm102@gmail.com";
+        String subject = "attachment test";
+        String text = "hello";
+        String pathToAttachment = "/Users/huekim/Desktop/pod_example1.png";
+
+        // when :
+        sut.sendMessage(to, subject, text, pathToAttachment);
+
+        // then :
+    }
+
+    @Test
+    public void 단일_수신자에게_다건의_첨부파일_전송시_성공() {
+        // given :
+        String to = "opklnm102@gmail.com";
+        String subject = "attachment test";
+        String text = "hello";
+        List<String> pathToAttachments = Arrays.asList("/Users/huekim/Desktop/pod_example1.png",
+                "/Users/huekim/Desktop/istio_overview.png");
+
+        // when :
+        sut.sendMessage(to, subject, text, pathToAttachments);
+
+        // then :
+    }
+
+    @Test
+    public void 여러_수신자에게_단건의_첨부파일_전송시_성공() {
+        // given :
+        List<String> to = Arrays.asList("opklnm102@gmail.com", "opklnm102@naver.com");
+        String subject = "attachment test";
+        String text = "hello";
+        String pathToAttachment = "/Users/huekim/Desktop/pod_example1.png";
+
+        // when :
+        sut.sendMessage(to, subject, text, pathToAttachment);
+
+        // then :
+    }
+
+    @Test
+    public void 여러_수신자에게_다건의_첨부파일_전송시_성공() {
+        // given :
+        List<String> to = Arrays.asList("opklnm102@gmail.com", "opklnm102@naver.com");
+        String subject = "attachment test";
+        String text = "hello";
+        List<String> pathToAttachments = Arrays.asList("/Users/huekim/Desktop/pod_example1.png",
+                "/Users/huekim/Desktop/istio_overview.png");
+
+        // when :
+        sut.sendMessage(to, subject, text, pathToAttachments);
+
+        // then :
+    }
+}
